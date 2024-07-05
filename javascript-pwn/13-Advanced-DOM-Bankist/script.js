188180570d4bcb0d4de97c79f506684b2bdfcb5c
platowantnothing <platowantnothing@outlook.com>
2024-07-05 17:54:36 +0800

    20240705

--- javascript-pwn/13-Advanced-DOM-Bankist/script.js
+++ javascript-pwn/13-Advanced-DOM-Bankist/script.js
@@ -39,106 +39,129 @@ document.addEventListener('keydown', function (e) {
 //**1 How the DOM Works  -- README.md*/
 
 //**2 Select, Create, and Delete elements */
-// 1 Selecting elements
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
+// // 1 Selecting elements
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
 
-const header = document.querySelector('.header');
-document.querySelector('.body');
+// const header = document.querySelector('.header'); // class -> .className
+// document.querySelector('.body');
 
-const allSections = document.querySelectorAll('.section');
-console.log(allSections);
+// const allSections = document.querySelectorAll('.section');
+// console.log(allSections);
 
-document.getElementById('section--1');
-//NOTE getElementsByTagName (live HTML elements) will automatically update HTML elements
-const allButtons = document.getElementsByTagName('button');
-console.log(allButtons);
+// document.getElementById('section--1');
+// //NOTE: getElementsByTagName (live HTML elements) will automatically update HTML elements
+// const allButtons = document.getElementsByTagName('button');
+// console.log(allButtons);
 
-console.log(document.getElementsByClassName('btn')); //NOTE return live HTML elements
+// console.log(document.getElementsByClassName('btn')); //NOTE: return live HTML elements
 
-// 2 Creating and inserting elements
-// NOTE review, about create html elements
-// .insertAdjacentHTML
+// // 2 Creating and inserting elements
+// // NOTE: review, about create html elements
+// // .insertAdjacentHTML
 
-const messgae = document.createElement('div'); //return a DOM element. Just store, if we want to use it, need insert
-messgae.classList.add('cookie-message'); // add class for the DOM
-// messgae.textContent = 'We provide cookiesd for improve analytics.'; // insert text
-messgae.innerHTML =
-  'We provide cookiesd for improve analytics. <button class="btn btn--close--cookie">Got it!</button>'; // Insert html
+// const messgae = document.createElement('div'); //return a DOM element. Just store, if we want to use it, need insert
+// messgae.classList.add('cookie-message'); // add class for the DOM
+// // messgae.textContent = 'We provide cookiesd for improve analytics.'; // insert text
+// messgae.innerHTML =
+//   'We provide cookiesd for improve analytics. <button class="btn btn--close--cookie">Got it!</button>'; // Insert html
 
-//insert dom into HTML
-// header.prepend(messgae); //NOTE header.prepend() under the <header>
-header.append(messgae); // NOTE header.append() above the </header>
-// NOTE the DOM cannot appear twice, DOM element is unique
-// header.append(messgae.cloneNode(true));
+// //insert dom into HTML
+// // header.prepend(messgae); //NOTE: header.prepend() under the <header>
+// header.append(messgae); // NOTE: header.append() above the </header>
+// // NOTE: the DOM cannot appear twice, DOM element is unique
+// // header.append(messgae.cloneNode(true));
 
-// header.before(messgae);
-// header.after(messgae);
+// // header.before(messgae);
+// // header.after(messgae);
 
-// 3 Delete elements
-document
-  .querySelector('.btn--close--cookie')
-  .addEventListener('click', function () {
-    // messgae.remove(); //this is new feature launched soon
-    //old-way
-    messgae.parentElement.removeChild(messgae); // Select the parent element then delete the child element
-  });
+// // 3 Delete elements
+// document
+//   .querySelector('.btn--close--cookie')
+//   .addEventListener('click', function () {
+//     // messgae.remove(); //this is new feature launched soon
+//     //old-way
+//     messgae.parentElement.removeChild(messgae); // Select the parent element then delete the child element
+//   });
 
 //**3 STYLES, ATTRIBUTES AND CLASSES */
 
-//1 Styles
-// NOTE Styles is incline style, only get value which automatically set, instead of default and non-exist value
-messgae.style.backgroundColor = '#37383d';
-messgae.style.width = '120%';
+// //1 Styles
+// // NOTE: Styles is incline style, only get value which automatically set, instead of default and non-exist value
+// messgae.style.backgroundColor = '#37383d';
+// messgae.style.width = '120%';
 
-// console.log(messgae.style.height); //empty string
-// console.log(messgae.style.width); //120%
+// // console.log(messgae.style.height); //empty string
+// // console.log(messgae.style.width); //120%
 
-// NOTE get deflaut style setting, and value retrieve from browser
-console.log(getComputedStyle(messgae).height);
+// // NOTE: get deflaut style setting, and value retrieve from browser
+// console.log(getComputedStyle(messgae).height);
 
-messgae.style.height =
-  Number.parseFloat(getComputedStyle(messgae).height, 10) + 40 + 'px';
+// messgae.style.height =
+//   Number.parseFloat(getComputedStyle(messgae).height, 10) + 40 + 'px';
 
-// NOTE change variable in CSS properties
-document.documentElement.style.setProperty('--color-primary', 'orangered'); // .setProperty(propertyName, value)
+// // NOTE: change variable in CSS properties
+// document.documentElement.style.setProperty('--color-primary', 'orangered'); // .setProperty(propertyName, value)
 
-//2 ATTRIBUTES
-const logo = document.querySelector('.nav__logo');
-console.log(logo.alt);
-console.log(logo.src);
-console.log(logo.className);
+// //2 ATTRIBUTES
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.src);
+// console.log(logo.className);
 
-// reset the attr value
-logo.alt = 'Beautiful minimalist logo';
-console.log(logo.alt);
+// // reset the attr value
+// logo.alt = 'Beautiful minimalist logo';
+// console.log(logo.alt);
 
-// Non-standard
-console.log(logo.designer); // undefined
-console.log(logo.getAttribute('designer')); // Jolin >> get the non-standard value
-logo.setAttribute('company', 'the Bankist');
+// // Non-standard
+// console.log(logo.designer); // undefined
+// console.log(logo.getAttribute('designer')); // Jolin >> get the non-standard value
+// logo.setAttribute('company', 'the Bankist');
 
-// NOTE the whole path vs. the relative path
-console.log(logo.src);
-console.log(logo.getAttribute('src')); //img/logo.png >> relative path
+// // NOTE: the whole path vs. the relative path
+// console.log(logo.src);
+// console.log(logo.getAttribute('src')); //img/logo.png >> relative path
 
-const link = document.querySelector('.nav__link--btn');
-console.log(link.href);
-console.log(link.getAttribute('href')); //#  >> relative path
+// const link = document.querySelector('.nav__link--btn');
+// console.log(link.href);
+// console.log(link.getAttribute('href')); //#  >> relative path
 
-// DATA Attributes format
-/**
- * NOTE
- *  The dataset read-only property of the HTMLElement interface provides read/write access to custom data attributes (data-*) on elements. It exposes a map of strings (DOMStringMap) with an entry for each data-* attribute.
- * https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dataset
- */
-console.log(logo.dataset.versionNumber); // index.html >> data-version-number
+// // DATA Attributes format
+// /**
+//  * NOTE:
+//  *  The dataset read-only property of the HTMLElement interface provides read/write access to custom data attributes (data-*) on elements. It exposes a map of strings (DOMStringMap) with an entry for each data-* attribute.
+//  * https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dataset
+//  */
+// console.log(logo.dataset.versionNumber); // index.html >> data-version-number
 
-// 3 Classes
+// // 3 Classes
 
-// use the classList API to remove and add classes
-logo.classList.add('anotherclass', 'fakeClass');
-logo.classList.remove('fakeClass');
-logo.classList.toggle('fakeClass'); // if fakeClass is set remove it, otherwise add it
-console.log(logo.classList.contains('anotherclass')); //not include
+// // use the classList API to remove and add classes
+// logo.classList.add('anotherclass', 'fakeClass');
+// logo.classList.remove('fakeClass');
+// logo.classList.toggle('fakeClass'); // if fakeClass is set remove it, otherwise add it
+// console.log(logo.classList.contains('anotherclass')); //not include
+
+//**4 Implement smooth scrolling */
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  // get the COORDINATE to scroll
+  const s1ccords = section1.getBoundingClientRect();
+  // console.log(s1ccords);
+
+  // console.log(e.target.getBoundingClientRect()); // console.log(this.getBoundingClientRect()); // e.target() same as this. which means that bottom coordinate
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, pageYOffset);
+
+  // console.log(
+  //   'height/width viewpoint:',
+  //   document.documentElement.clientHeight,
+  //   document.documentElement.clientWidth
+  // );
+
+  // // Scrolling
+  // window.scrollTo(s1ccords.left, s1ccords.top);
+});
